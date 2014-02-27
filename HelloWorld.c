548f60262009bd112d50b5237023f244f43a6615
daksheshvyaslive <daksheshvyas@live.com>
2014-02-27 12:38:14 -0800

    print detail messages

--- HelloWorld.c
+++ HelloWorld.c
@@ -7,7 +7,7 @@ int main(){
         for(i=0;i<x;i++)
                 printf("Hello World");
 
-        printf("Hello World printed %d times",x);
+        printf("Hello World printed %d times now",x);
 
         return 0;
 
